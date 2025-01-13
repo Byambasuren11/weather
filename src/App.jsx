1424fece20e944b93673297fc5c8d75d02534482
Byambasuren11 <byumbasurenbold@gmail.com>
2025-01-13 18:31:04 +0800

    ho

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+//import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
+      
+      <div className="flex justify-center items-start w-full h-screen bg-gray-800 relative">
+          <div className="flex justify-start items-start w-1/2 h-screen bg-gray-100 ">
+
+          </div>
+          <div className="flex justify-end items-end w-1/2 h-screen bg-black ">
+
+          </div>
+          <div className="rounded-full w-96 h-96 border border-white absolute z-1">
+            <div>
+              <div>
+
+              </div>
+            </div>
+          </div>
+        </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
