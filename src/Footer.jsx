36fe6618f1a0cf5811e809ebe3510145b3efea57
Byambasuren11 <byumbasurenbold@gmail.com>
2025-01-20 18:14:19 +0800

    hoho

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,18 +2,13 @@ import Home from "./icons/Home";
 import Location from "./icons/Location";
 import Heart from "./icons/Heart";
 import Person from "./icons/Person";
-//import Skeleton from "react-loading-skeleton";
-import Loader from "./Skelton-Loader";
 
-const onClick=()=>{
- console.log("hoho")
-}
 const Footer = (props) => {
   return (
     <div className="flex justify-around gap-14">
       <Home/>
       <Location/>
-      <Heart onClick={onClick}/>
+      <Heart/>
       <Person/>
     </div>
   );
--- src/icons/Heart.jsx
+++ src/icons/Heart.jsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 const Heart = (props) => (
-  <div className=" ">
   
   <svg
     xmlns="http://www.w3.org/2000/svg"
@@ -19,10 +18,6 @@ const Heart = (props) => (
       d="M15.916 7.369a1 1 0 0 0 1.501 0 7 7 0 0 1 10.83 8.858l-6.06 8a7 7 0 0 1-11.2-.054l-5.94-8a7 7 0 0 1 10.869-8.804"
     ></path>
   </svg>
-  {/* <div className="w-[150px] h-[150px]">
-x
-  </div> */}
-  </div>
 );
 
 export default Heart;
