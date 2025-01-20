eeb10df6d61dc18fc4d890c67cfd427afa018079
Byambasuren11 <byumbasurenbold@gmail.com>
2025-01-20 17:23:40 +0800

    h

--- src/Emoji.jsx
+++ src/Emoji.jsx
@@ -2,39 +2,39 @@ const Emoji = (props) => {
   const { typeOfWeatherDay } = props;
   switch (typeOfWeatherDay) {
     case "Sunny":
-      return <img className="w-[400px]" src="./public/sunnyDay.webp" />;
+      return <img className="w-[400px]" src="./sunnyDay.webp" />;
     case "Clear":
-      return <img className="w-[400px]" src="./public/sunnyDay.webp" />;
+      return <img className="w-[400px]" src="./sunnyDay.webp" />;
     case "Partly Cloudy ":
-        return <img src="./public/partlyCloudyDay.webp" />;
+        return <img src="./partlyCloudyDay.webp" />;
     case "Cloudy ":
-        return <img src="./public/partlyCloudyDay.webp" />;
+        return <img src="./partlyCloudyDay.webp" />;
     case "Overcast ":
-        return <img src="./public/partlyCloudyDay.webp" />;
+        return <img src="./partlyCloudyDay.webp" />;
     case "Windy":
-        return  <img src="./public/WindDay.webp" />;
+        return  <img src="./WindDay.webp" />;
     case "Patchy rain nearby":
-        return <img src="./public/RainDay.webp" />;
+        return <img src="./RainDay.webp" />;
     case "Light rain shower":
-        return <img src="./public/RainDay.webp" />;
+        return <img src="./RainDay.webp" />;
     case "Mist":
-        return <img src="./public/RainDay.webp" />;  
+        return <img src="./RainDay.webp" />;  
     case "Light freezing rain":
-            return <img src="./public/RainDay.webp" />;    
+            return <img src="./RainDay.webp" />;    
     case "Patchy moderate snow":
-        return <img src="./public/SnowDay.webp" />;
+        return <img src="./SnowDay.webp" />;
     case "Moderate snow":
-        return <img src="./public/SnowDay.webp" />;
+        return <img src="./SnowDay.webp" />;
     case "Light snow":
-        return <img src="./public/SnowDay.webp" />; 
+        return <img src="./SnowDay.webp" />; 
     case "Heavy snow":
-        return <img src="./public/SnowDay.webp" />;
+        return <img src="./SnowDay.webp" />;
     case "Blowing snow":
-        return <img src="./public/SnowDay.webp" />;
+        return <img src="./SnowDay.webp" />;
     case "Moderate or heavy snow showers":
-        return <img src="./public/SnowDay.webp" />; 
+        return <img src="./SnowDay.webp" />; 
     default:
-      return <img className="w-[400px]" src="./public/sunnyDay.webp" />;
+      return <img className="w-[400px]" src="./sunnyDay.webp" />;
   }
 };
 export default Emoji;
--- src/EmojiNight.jsx
+++ src/EmojiNight.jsx
@@ -3,39 +3,39 @@ const Emoji = (props) => {
     
     switch (typeOfWeatherNight) {
       case "Sunny":
-        return <img className="w-[400px]" src="./public/sunnyNight.webp" />;
+        return <img className="w-[400px]" src="./sunnyNight.webp" />;
       case "Clear":
-        return <img className="w-[400px]" src="./public/sunnyNight.webp" />;
+        return <img className="w-[400px]" src="./sunnyNight.webp" />;
       case "Partly Cloudy ":
-          return <img src="./public/partlyCloudyNight.webp" />;
+          return <img src="./partlyCloudyNight.webp" />;
       case "Cloudy ":
-          return <img src="./public/partlyCloudyNight.webp" />;
+          return <img src="./partlyCloudyNight.webp" />;
       case "Overcast ":
-          return <img src="./public/partlyCloudyNight.webp" />;
+          return <img src="./partlyCloudyNight.webp" />;
       case "Windy":
-          return  <img src="./public/WindNight.webp" />;
+          return  <img src="./WindNight.webp" />;
       case "Patchy rain nearby":
-          return <img src="./public/RainNight.webp" />;
+          return <img src="./RainNight.webp" />;
       case "Light rain shower":
-          return <img src="./public/RainNight.webp" />;
+          return <img src="./RainNight.webp" />;
       case "Mist":
-          return <img src="./public/RainNight.webp" />;  
+          return <img src="./RainNight.webp" />;  
       case "Light freezing rain":
-              return <img src="./public/RainNight.webp" />;    
+              return <img src="./RainNight.webp" />;    
       case "Patchy moderate snow":
-          return <img src="./public/SnowNight.webp" />;
+          return <img src="./SnowNight.webp" />;
       case "Moderate snow":
-          return <img src="./public/SnowNight.webp" />;
+          return <img src="./SnowNight.webp" />;
       case "Light snow":
-          return <img src="./public/SnowNight.webp" />; 
+          return <img src="./SnowNight.webp" />; 
       case "Heavy snow":
-          return <img src="./public/SnowNight.webp" />;
+          return <img src="./SnowNight.webp" />;
       case "Blowing snow":
-          return <img src="./public/SnowNight.webp" />;
+          return <img src="./SnowNight.webp" />;
       case "Moderate or heavy snow showers":
-          return <img src="./public/SnowNight.webp" />; 
+          return <img src="./SnowNight.webp" />; 
       default:
-        return <img className="w-[400px]" src="./public/sunnyNight.webp" />;
+        return <img className="w-[400px]" src="./sunnyNight.webp" />;
     }
   };
   export default Emoji;
